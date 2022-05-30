7a13db64929a45bcea709bb11d1bfa74bc7ab558
Moritz Friedrich <moritz@matchory.com>
2022-05-30 08:55:25 +0200

    Switched to build pipeline

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+export default {
+    root: 'src/',
+    envDir: '../',
+
+    build: {
+        outDir: '../dist',
+        sourcemap: true,
+        manifest: true,
+        emptyOutDir: true,
+        reportCompressedSize: true
+    }
+}
