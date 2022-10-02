5b3c72d569787018eb29fc517664a6b6fdd4a6be
Moritz Friedrich <moritz@matchory.com>
2022-10-02 14:26:12 +0200

    Vite v3, GA, New Font/Design, PostCSS

---
+++ postcss.config.ts
@@ -0,0 +1,8 @@
+import postcssNesting from 'postcss-nesting';
+
+export default {
+    map: true,
+    plugins: [
+        postcssNesting( {} )
+    ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-export default {
-    root: 'src/',
-    envDir: '../',
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-    build: {
-        outDir: '../dist',
-        sourcemap: true,
-        manifest: true,
-        emptyOutDir: true,
-        reportCompressedSize: true
-    }
-}
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        root: 'src/',
+        envDir: '../',
+        base: '/',
+
+        build: {
+            outDir: '../dist',
+            sourcemap: true,
+            manifest: true,
+            emptyOutDir: true,
+            reportCompressedSize: true,
+        },
+
+        plugins: [
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        googleTagId: env.VITE_ENV_GOOGLE_TAG_ID,
+                        primaryColor: 'rgb(202, 194, 188)',
+                    },
+                },
+            }),
+        ],
+    };
+});
