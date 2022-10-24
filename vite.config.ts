79825319350b4243dab27f91352af7e9f7dea04a
Moritz Friedrich <moritz@matchory.com>
2022-10-24 17:33:48 +0200

    Added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { readFile } from 'fs/promises';
 import { defineConfig, loadEnv } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { LocalBusiness, Thing } from 'schema-dts';
-import { resolve } from 'path'
+import { sitemap } from './src/plugins/vite-plugin-sitemap';
+import { mpa } from './src/plugins/vite-plugin-mpa';
+import { join, parse, resolve } from 'path';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 export default defineConfig(async ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
@@ -37,14 +39,25 @@ export default defineConfig(async ({ mode }) => {
             manifest: true,
             emptyOutDir: true,
             reportCompressedSize: true,
-
             rollupOptions: {
+                /*
+                input: {
+                    'main': resolve(__dirname + '/src/pages/index.html'),
+                    'pages/x/contact': resolve(
+                        __dirname + '/src/pages/contact.html',
+                    ),
+                    'privacy': resolve(__dirname + '/src/pages/privacy.html'),
+                    'imprint': resolve(__dirname + '/src/pages/imprint.html'),
+                },
                 input: {
                     main: resolve(__dirname + '/src/pages/index.html'),
-                    contact: resolve(__dirname + '/src/pages/contact.html'),
-                    privacy: resolve(__dirname + '/src/pages/privacy.html'),
-                    imprint: resolve(__dirname + '/src/pages/imprint.html'),
-                },
+                    ...entryPoints(
+                        'src/pages/contact.html',
+                        'src/pages/privacy.html',
+                        'src/pages/imprint.html',
+                    )
+                }
+*/
             },
         },
 
@@ -76,29 +89,37 @@ export default defineConfig(async ({ mode }) => {
         },
 
         plugins: [
-            createHtmlPlugin({
-                pages: [
-                    {
-                        entry: 'src/main.ts',
-                        filename: 'index.html',
-                        template: 'pages/index.html',
-                    },
-                    {
-                        entry: 'src/main.ts',
-                        filename: 'pages/contact.html',
-                        template: 'pages/contact.html',
-                    },
-                    {
-                        entry: 'src/main.ts',
-                        filename: 'pages/privacy.html',
-                        template: 'pages/privacy.html',
-                    },
-                    {
-                        entry: 'src/main.ts',
-                        filename: 'pages/imprint.html',
-                        template: 'pages/imprint.html',
-                    },
-                ],
+            mpa({}),
+
+            sitemap({
+                baseUrl: schema.url,
+            }),
+
+            ViteEjsPlugin({
+                schema,
+                vars: {
+                    primaryColor: 'rgb(202, 194, 188)',
+                    localBusinessSchema: schema,
+                    geoMapId: env.VITE_GEO_MAP_ID,
+                    googleTagId: env.VITE_GOOGLE_TAG_ID,
+                    googleTagEnabled: flagEnabled(
+                        env.VITE_GOOGLE_TAG_ENABLED?.toLowerCase(),
+                    ),
+                    footerLinks: [
+                        {
+                            href: '/contact.html',
+                            label: 'Kontakt',
+                        },
+                        {
+                            href: '/privacy.html',
+                            label: 'Datenschutz',
+                        },
+                        {
+                            href: '/imprint.html',
+                            label: 'Impressum',
+                        },
+                    ],
+                },
             }),
         ],
     };
@@ -112,4 +133,16 @@ async function loadSchema<T extends Thing>(path: string): Promise<T> {
     const schema = await readFile(path, 'utf-8');
 
     return JSON.parse(schema);
+}
+
+function entryPoints(...paths: string[]): Record<string, string> {
+    const entries = paths.map(parse).map((entry) => {
+        const { dir, base, name } = entry;
+        const key = join(dir, name);
+        const path = resolve(__dirname, dir, base);
+
+        return [key, path];
+    });
+
+    return Object.fromEntries(entries);
 }
