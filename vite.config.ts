fa779472368140f89402dae23ab4c8fa13ceab2c
Moritz Friedrich <moritz@matchory.com>
2023-11-26 22:05:10 +0100

    Fixed web font configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,36 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import { defineConfig } from "vite";
-import fontDownload from "vite-plugin-webfont-dl";
-import storyblokComponentsPlugin from "./src/build/vite-plugin-storyblok-components";
-import storyblokRedirectsPlugin from "./src/build/vite-plugin-storyblok-redirects.mjs";
+import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from 'vite';
+import fontDownload from 'vite-plugin-webfont-dl';
+import storyblokComponentsPlugin from './src/build/vite-plugin-storyblok-components';
+import storyblokRedirectsPlugin from './src/build/vite-plugin-storyblok-redirects.mjs';
 
 export default defineConfig({
-  plugins: [
-    basicSsl(),
-    storyblokComponentsPlugin({
-      componentsPath: "src/lib/components/Blocks",
-    }),
-    storyblokRedirectsPlugin({
-      datasource: 'redirects'
-    }),
-    sveltekit(),
-    fontDownload(),
-  ],
+	plugins: [
+		basicSsl(),
+		storyblokComponentsPlugin({
+			componentsPath: 'src/lib/components/Blocks'
+		}),
+		storyblokRedirectsPlugin({
+			datasource: 'redirects'
+		}),
+		sveltekit(),
+		fontDownload([
+			'https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&family=Source+Sans+3:ital,wght@0,300;0,400;1,300;1,400&display=swap'
+		])
+	],
 
-  server: {
-    https: true,
-    strictPort: true,
-  },
+	server: {
+		https: true,
+		strictPort: true
+	},
 
-  build: {
-    sourcemap: true,
-    reportCompressedSize: true,
-  },
+	build: {
+		sourcemap: true,
+		reportCompressedSize: true
+	},
 
-  css: {
-    devSourcemap: true,
-  },
+	css: {
+		devSourcemap: true
+	}
 });
