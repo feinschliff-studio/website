71d7a4253d42c5be82989cfc4104b9ec30f3d148
Moritz Friedrich <moritz@matchory.com>
2023-11-26 14:48:32 +0100

    Added environment name

--- svelte.config.js
+++ svelte.config.js
@@ -1,36 +1,44 @@
-import adapter from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { env } from 'node:process';
 
-/** @type {import("@sveltejs/kit").Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess({
-  }),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess({}),
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
 
-    alias: {
-      "$routes": "./src/routes",
-      "@storyblok/svelte": "./node_modules/@storyblok/svelte",
-      "$storyblok": ".storyblok",
-      "$storyblok/*": ".storyblok/*",
-    },
+		version: {
+			name: env.NODE_ENV === 'production'
+				? env.CF_PAGES_BRANCH === 'main'
+					? 'production'
+					: 'preview'
+				: 'development'
+		},
 
-    prerender: {
-      crawl: true,
-      concurrency: 20,
-      handleHttpError: "fail",
-    },
-  },
-  compilerOptions: {
-    enableSourcemap: true,
-    sourcemap: true,
-  },
+		alias: {
+			'$routes': './src/routes',
+			'@storyblok/svelte': './node_modules/@storyblok/svelte',
+			'$storyblok': '.storyblok',
+			'$storyblok/*': '.storyblok/*'
+		},
+
+		prerender: {
+			crawl: true,
+			concurrency: 20,
+			handleHttpError: 'fail'
+		}
+	},
+	compilerOptions: {
+		enableSourcemap: true,
+		sourcemap: true
+	}
 };
 
 export default config;
