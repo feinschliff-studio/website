d500bc14b440841f858f3fe56fdabf58583fbddf
Moritz Friedrich <moritz@matchory.com>
2023-11-28 14:34:55 +0100

    Optimize dynamic imports and clean up code
    Refactored code to handle Storyblok imports dynamically and removed unused variables, functions, and components. This leads to cleaner, more readable code and potentially better performance due to the dynamic imports only loading when needed.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,42 +1,14 @@
-import { dev, version } from "$app/environment";
 import { PUBLIC_STORYBLOK_ACCESS_TOKEN } from "$env/static/public";
+import type { LayoutLoad } from "./$types";
 import { init } from "$lib/storyblok";
-import type { CookieBannerStoryblok, SiteConfigStoryblok } from "$storyblok/components";
-import type { StoryblokClient } from "@storyblok/svelte";
-import type { Load } from "@sveltejs/kit";
 
 export const ssr = true;
 
-/**
- * If we're running in the browser, and the app is either in development mode or rendering a preview,
- * initialize the Storyblok client. This allows to update the preview in real-time.
- */
-export const load: Load = async function load() {
+export const load: LayoutLoad = async function load({ data }) {
   const storyblokClient = await init(PUBLIC_STORYBLOK_ACCESS_TOKEN);
-  const [cookieBanner, siteConfig] = await Promise.all([
-    loadCookieBanner(storyblokClient),
-    loadSiteConfig(storyblokClient),
-  ]);
 
   return {
+    ...data,
     storyblokClient,
-    cookieBanner,
-    siteConfig,
   };
 };
-
-async function loadSiteConfig(storyblokClient: StoryblokClient): Promise<SiteConfigStoryblok> {
-  const response = await storyblokClient.get("cdn/stories/_settings", {
-    version: dev || version.startsWith("preview") ? "draft" : "published",
-  });
-
-  return response.data.story.content;
-}
-
-async function loadCookieBanner(storyblokClient: StoryblokClient): Promise<CookieBannerStoryblok> {
-  const response = await storyblokClient.get("cdn/stories/_cookie-banner", {
-    version: dev || version.startsWith("preview") ? "draft" : "published",
-  });
-
-  return response.data.story.content;
-}
